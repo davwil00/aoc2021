1cfb2f2f1a2795424dd57157a92f74426a427dcd
David Williams <davwil00@gmail.com>
2021-12-01 07:25:18 +0000

    Update utils

--- src/main/kotlin/utils/Utils.kt
+++ src/main/kotlin/utils/Utils.kt
@@ -1,4 +1,12 @@
 package utils
 
-class Utils {
+import java.io.File
+
+fun readInput(day: Int) : String {
+    val paddedDay = day.toString().padStart(2, '0')
+    return File("src/main/resources/day$paddedDay/input.txt").readText().trim()
+}
+
+fun readInputLines(day: Int) : List<String> {
+    return readInput(day).lines()
 }
