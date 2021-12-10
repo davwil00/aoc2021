9ab47fb575d0db7d5b768de8831d81efaa6e0f14
David Williams <davwil00@gmail.com>
2021-12-10 22:51:31 +0000

    Make day 9 much nicer

--- src/main/kotlin/day09/SmokeBasin.kt
+++ src/main/kotlin/day09/SmokeBasin.kt
@@ -1,40 +1,42 @@
 package day09
 
+import utils.Coordinate
 import utils.product
 import utils.readInputLines
 import utils.splitToString
 
 class SmokeBasin {
 
-    fun produceMap(input: List<String>): List<List<Int>> {
-        return input.map { line -> line.splitToString().map { it.toInt() } }
+    fun produceMap(input: List<String>): HeightMap {
+        val map = input.flatMapIndexed { rowIdx, line ->
+            line.splitToString().withIndex().associate { (colIdx, height) ->
+                Pair(Coordinate(rowIdx, colIdx), height.toInt())
+            }.entries
+        }.associate { it.key to it.value }
+
+        return HeightMap(map, input[0].length, input.size)
     }
 
-    fun findRiskLevelsOfLowPoints(map: List<List<Int>>): Int {
-        return map.foldIndexed(0) { rowIdx, total, row ->
-            return@foldIndexed row.foldIndexed(total) colTotal@ { colIdx, rowTotal, height ->
-                return@colTotal if (height < (getAdjacentValues(rowIdx, colIdx, map).minOrNull() ?: height)) {
-                    rowTotal + height + 1
-                } else {
-                    rowTotal
-                }
+    fun findRiskLevelsOfLowPoints(heightMap: HeightMap): Int {
+        return heightMap.map.entries.fold(0) { total, (coordinate, height) ->
+            if (height < (getAdjacentHeights(coordinate, heightMap).minOrNull()!!)) {
+                total + height + 1
+            } else {
+                total
             }
         }
     }
 
-    fun findBasin(map: List<List<Int>>): Long {
-        val assignedBasins = mutableMapOf<Pair<Int, Int>, Int>()
+    fun findBasin(heightMap: HeightMap): Long {
+        val assignedBasins = mutableMapOf<Coordinate, Int>()
         var basinNo = 0
-        map.forEachIndexed { rowIdx, row ->
-            row.forEachIndexed { colIdx, height ->
-                if (height != 9 && !assignedBasins.contains(Pair(rowIdx, colIdx))) {
-                    findSurroundingNonNines(map, rowIdx, colIdx, assignedBasins, basinNo)
-                    basinNo++
-                }
+        heightMap.map.forEach { (coordinate, height) ->
+            if (height != 9 && coordinate !in assignedBasins) {
+                findSurroundingNonNines(heightMap, coordinate, assignedBasins, basinNo)
+                basinNo++
             }
         }
         return assignedBasins.values
-            .filter { it != 0 }
             .groupingBy { it }
             .eachCount()
             .values
@@ -43,69 +45,28 @@ class SmokeBasin {
             .product()
     }
 
-    fun findSurroundingNonNines(map: List<List<Int>>, rowIdx: Int, colIdx: Int, assignedBasins: MutableMap<Pair<Int, Int>, Int>, basinNo: Int) {
-        assignedBasins[Pair(rowIdx, colIdx)] = basinNo
-        val toProcess = mutableSetOf<Pair<Int, Int>>()
-        // check left
-        for (it in colIdx - 1 downTo 0) {
-            if (map[rowIdx][it] == 9 || assignedBasins.contains(Pair(rowIdx, it))) {
-                break
-            } else {
-                toProcess.add(Pair(rowIdx, it))
-            }
-        }
-
-        // check right
-        for (it in colIdx + 1 until map[rowIdx].size) {
-            if (map[rowIdx][it] == 9 || assignedBasins.contains(Pair(rowIdx, it))) {
-                break
-            } else {
-                toProcess.add(Pair(rowIdx, it))
-            }
-        }
-
-        // check up
-        for (it in rowIdx - 1 downTo 0) {
-            if (map[it][colIdx] == 9 || assignedBasins.contains(Pair(it, colIdx))) {
-                break
-            } else {
-                toProcess.add(Pair(it, colIdx))
-            }
-        }
-
-        // check down
-        for (it in rowIdx + 1 until map.size) {
-            if (map[it][colIdx] == 9 || assignedBasins.contains(Pair(it, colIdx))) {
-                break
-            } else {
-                toProcess.add(Pair(it, colIdx))
-            }
-        }
-
-        toProcess.forEach { (newRowIdx, newColIdx) ->
-            if (!assignedBasins.contains(Pair(newRowIdx, newColIdx))) {
-                findSurroundingNonNines(map, newRowIdx, newColIdx, assignedBasins, basinNo)
-            }
-        }
+    private fun getAdjacentHeights(coordinate: Coordinate, map: HeightMap): Sequence<Int> {
+        return coordinate.getAdjacentCoordinates().mapNotNull { map[it] }
     }
 
-    private fun getAdjacentValues(rowIdx: Int, colIdx: Int, map: List<List<Int>>): List<Int> {
-        val vals = mutableListOf<Int>()
-        if (colIdx != 0) {
-            vals.add(map[rowIdx][colIdx - 1])
-        }
-        if (colIdx < map[rowIdx].size - 1) {
-            vals.add(map[rowIdx][colIdx + 1])
-        }
-        if (rowIdx != 0) {
-            vals.add(map[rowIdx - 1][colIdx])
-        }
-        if (rowIdx < map.size - 1) {
-            vals.add(map[rowIdx + 1][colIdx])
-        }
-        return vals
+    private fun findSurroundingNonNines(heightMap: HeightMap, coordinate: Coordinate, assignedBasins: MutableMap<Coordinate, Int>, basinNo: Int) {
+        assignedBasins[coordinate] = basinNo
+        coordinate.getAdjacentCoordinates()
+            .filter(heightMap::isInBoundary)
+            .filterNot(heightMap::isBorder)
+            .filterNot(assignedBasins::contains)
+            .forEach { adjacentCoordinate ->
+                findSurroundingNonNines(heightMap, adjacentCoordinate, assignedBasins, basinNo)
+            }
+    }
+
+    class HeightMap(val map: Map<Coordinate, Int>, private val maxY: Int, private val maxX: Int) {
+        operator fun get(coordinate: Coordinate) = map[coordinate]
+        fun isBorder(coordinate: Coordinate) = map[coordinate] == 9
+        fun isInBoundary(coordinate: Coordinate) = coordinate.x < maxX && coordinate.y < maxY
     }
 }
+
 
 fun main() {
     val input = readInputLines(9)
@@ -114,6 +75,5 @@ fun main() {
     val riskLevelsOfLowPoints = smokeBasin.findRiskLevelsOfLowPoints(map)
     println("Risk levels of low points is $riskLevelsOfLowPoints")
     val multiplied3LargestBasins = smokeBasin.findBasin(map)
-    println("Ans: $multiplied3LargestBasins") // 381300 too low // 617520 too low
-
+    println("Three largest basins: $multiplied3LargestBasins")
 }
