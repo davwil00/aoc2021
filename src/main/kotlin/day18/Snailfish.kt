ecb6167034f9797f78ff2c67a4a735ef397f8ee3
David Williams <davwil00@gmail.com>
2021-12-30 22:47:31 +0000

    Day 18 - part 2

--- src/main/kotlin/day18/Snailfish.kt
+++ src/main/kotlin/day18/Snailfish.kt
@@ -1,7 +1,8 @@
 package day18
 
-import day18.SnailfishNumber.Direction.LEFT
-import day18.SnailfishNumber.Direction.RIGHT
+import day18.Snailfish.SnailfishNumber.Direction.LEFT
+import day18.Snailfish.SnailfishNumber.Direction.RIGHT
+import org.slf4j.LoggerFactory
 import utils.isDigits
 import utils.readInputLines
 import utils.splitToString
@@ -9,226 +10,252 @@ import java.util.*
 import kotlin.math.ceil
 import kotlin.math.floor
 
-fun addAndReduceList(snailfishNumbers: List<String>): SnailfishNumber {
-    return snailfishNumbers.map { SnailfishNumber.fromString(it) }
-        .reduce { first, second ->
-//            println("added = ${first + second}")
-            reduce(first + second)
+class Snailfish {
+
+    fun addAndReduceList(snailfishNumbers: List<String>): SnailfishNumber {
+        return snailfishNumbers.map { SnailfishNumber.fromString(it) }
+            .reduce { first, second ->
+                logger.debug("added = ${first + second}")
+                reduce(first + second)
+            }
+    }
+
+    fun reduce(snailfishNumber: SnailfishNumber): SnailfishNumber {
+        while(snailfishNumber.explode() || snailfishNumber.split()) {
+            logger.debug(snailfishNumber.toString())
         }
-}
 
-fun reduce(snailfishNumber: SnailfishNumber): SnailfishNumber {
-    while(snailfishNumber.explode() || snailfishNumber.split()) {
-//        println(snailfishNumber)
+        logger.debug("""
+            --------------Reduced-----------------
+            $snailfishNumber
+            --------------------------------------
+        """.trimIndent())
+        return snailfishNumber
     }
 
-//    println("""
-//        --------------------------------------
-//        $snailfishNumber
-//        --------------------------------------
-//    """.trimIndent())
-    return snailfishNumber
-}
-
-class SnailfishNumber(private var parent: SnailfishNumber?) {
-
-    constructor(parent: SnailfishNumber?, left: Int, right: Int): this(parent) {
-        this.leftValue = left
-        this.rightValue = right
-    }
-
-    var leftValue: Int? = null
-    var rightValue: Int? = null
-    var leftNode: SnailfishNumber? = null
-    var rightNode: SnailfishNumber? = null
-
-    private fun getLeft() = leftNode ?: leftValue
-
-    fun setLeft(left: Any) {
-        when (left) {
-            is Int -> leftValue = left
-            is SnailfishNumber -> leftNode = left
-            else -> throw IllegalArgumentException("Unexpected value type $left")
-        }
-    }
-
-    private fun getRight() = rightNode ?: rightValue
-
-    fun setRight(right: Any) {
-        when (right) {
-            is Int -> rightValue = right
-            is SnailfishNumber -> rightNode = right
-            else -> throw IllegalArgumentException("Unexpected value type $right")
-        }
-    }
-
-    private fun getDepth(): Int {
-        return if (this.parent == null) 0 else 1 + this.parent!!.getDepth()
-    }
-
-    fun explode(): Boolean {
-        if (this.getDepth() == 4) {
-            val nodeToExplode = this
-            val leafNodes = getAllLeafNodes()
-            val thisIdx = leafNodes.indexOfFirst { it.parent === nodeToExplode }
-            if (thisIdx > 0) {
-                val node = leafNodes[thisIdx - 1]
-                val newValue = node.value + nodeToExplode.leftValue!!
-                when (node.direction) {
-                    LEFT -> node.parent.leftValue = newValue
-                    RIGHT -> node.parent.rightValue = newValue
+    fun findLargestMagnitude(snailfishNumbers: List<String>): Int {
+        val first = snailfishNumbers.maxOf { snailfishNumber ->
+            snailfishNumbers
+                .filter { it != snailfishNumber }
+                .maxOf { other ->
+                    reduce(
+                        SnailfishNumber.fromString(snailfishNumber) +
+                        SnailfishNumber.fromString(other)
+                    ).getMagnitude()
                 }
+        }
+
+        return first
+    }
+
+    class SnailfishNumber(private var parent: SnailfishNumber?) {
+
+        constructor(parent: SnailfishNumber?, left: Int, right: Int): this(parent) {
+            this.leftValue = left
+            this.rightValue = right
+        }
+
+        var leftValue: Int? = null
+        var rightValue: Int? = null
+        var leftNode: SnailfishNumber? = null
+        var rightNode: SnailfishNumber? = null
+
+        private fun getLeft() = leftNode ?: leftValue
+
+        fun setLeft(left: Any) {
+            when (left) {
+                is Int -> leftValue = left
+                is SnailfishNumber -> leftNode = left
+                else -> throw IllegalArgumentException("Unexpected value type $left")
             }
-            if (thisIdx + 2 < leafNodes.size) {
-                val node = leafNodes[thisIdx + 2]
-                val newValue = nodeToExplode.rightValue!!
-                when (node.direction) {
-                    LEFT -> node.parent.leftValue = node.value + newValue
-                    RIGHT -> node.parent.rightValue = node.value + newValue
+        }
+
+        private fun getRight() = rightNode ?: rightValue
+
+        fun setRight(right: Any) {
+            when (right) {
+                is Int -> rightValue = right
+                is SnailfishNumber -> rightNode = right
+                else -> throw IllegalArgumentException("Unexpected value type $right")
+            }
+        }
+
+        private fun getDepth(): Int {
+            return if (this.parent == null) 0 else 1 + this.parent!!.getDepth()
+        }
+
+        fun explode(): Boolean {
+            if (this.getDepth() == 4) {
+                val nodeToExplode = this
+                val leafNodes = getAllLeafNodes()
+                val thisIdx = leafNodes.indexOfFirst { it.parent === nodeToExplode }
+                if (thisIdx > 0) {
+                    val node = leafNodes[thisIdx - 1]
+                    val newValue = node.value + nodeToExplode.leftValue!!
+                    when (node.direction) {
+                        LEFT -> node.parent.leftValue = newValue
+                        RIGHT -> node.parent.rightValue = newValue
+                    }
                 }
-            }
-            if (this.parent!!.leftNode === nodeToExplode) {
-                this.parent!!.leftNode = null
-                this.parent!!.leftValue = 0
-            } else {
-                this.parent!!.rightNode = null
-                this.parent!!.rightValue = 0
+                if (thisIdx + 2 < leafNodes.size) {
+                    val node = leafNodes[thisIdx + 2]
+                    val newValue = nodeToExplode.rightValue!!
+                    when (node.direction) {
+                        LEFT -> node.parent.leftValue = node.value + newValue
+                        RIGHT -> node.parent.rightValue = node.value + newValue
+                    }
+                }
+                if (this.parent!!.leftNode === nodeToExplode) {
+                    this.parent!!.leftNode = null
+                    this.parent!!.leftValue = 0
+                } else {
+                    this.parent!!.rightNode = null
+                    this.parent!!.rightValue = 0
+                }
+
+                logger.debug("Exploded: ")
+                return true
             }
 
-//            print("Exploded: ")
+            return (leftNode != null && leftNode!!.explode()) ||
+                    (rightNode != null && rightNode!!.explode())
+        }
+
+        fun split(): Boolean {
+            val node = getAllLeafNodes().firstOrNull { it.value >= 10 }
+            if (node != null) {
+                val newValue = split(node.value, node.parent)
+                when (node.direction) {
+                    LEFT -> {
+                        node.parent.leftValue = null
+                        node.parent.leftNode = newValue
+                    }
+                    RIGHT -> {
+                        node.parent.rightValue = null
+                        node.parent.rightNode = newValue
+                    }
+                }
+                return true
+            }
+            return false
+        }
+
+        fun getMagnitude(): Int {
+            val left = if (leftValue != null) leftValue!! * 3 else leftNode!!.getMagnitude() * 3
+            val right = if (rightValue != null) rightValue!! * 2 else rightNode!!.getMagnitude() * 2
+
+            return left + right
+        }
+
+        data class LeafNode(val value: Int, val parent: SnailfishNumber, val direction: Direction)
+        enum class Direction { LEFT, RIGHT }
+
+        private fun getAllLeafNodes(): List<LeafNode> {
+            if (this.parent != null) {
+                return parent!!.getAllLeafNodes()
+            }
+
+            return getChildLeafNodes()
+        }
+
+        private fun getChildLeafNodes(): List<LeafNode> {
+            val nodes = mutableListOf<LeafNode>()
+            if (this.leftNode != null) {
+                nodes.addAll(this.leftNode!!.getChildLeafNodes())
+            }
+            if (this.leftValue != null) {
+                nodes.add(LeafNode(this.leftValue!!, this, LEFT))
+            }
+            if (this.rightNode != null) {
+                nodes.addAll(this.rightNode!!.getChildLeafNodes())
+            }
+            if (this.rightValue != null) {
+                nodes.add(LeafNode(this.rightValue!!, this, RIGHT))
+            }
+
+            return nodes
+        }
+
+        operator fun plus(other: SnailfishNumber) : SnailfishNumber {
+            val newParent = SnailfishNumber(null)
+            newParent.setLeft(this)
+            this.parent = newParent
+            newParent.setRight(other)
+            other.parent = newParent
+
+            return newParent
+        }
+
+        override fun toString(): String {
+            return "[${this.getLeft()},${this.getRight()}]"
+        }
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as SnailfishNumber
+
+            if (leftValue != other.leftValue) return false
+            if (rightValue != other.rightValue) return false
+            if (leftNode != other.leftNode) return false
+            if (rightNode != other.rightNode) return false
+
             return true
         }
 
-        return (leftNode != null && leftNode!!.explode()) ||
-                (rightNode != null && rightNode!!.explode())
-    }
+        override fun hashCode(): Int {
+            var result = leftValue ?: 0
+            result = 31 * result + (rightValue ?: 0)
+            result = 31 * result + (leftNode?.hashCode() ?: 0)
+            result = 31 * result + (rightNode?.hashCode() ?: 0)
+            return result
+        }
 
-    fun split(): Boolean {
-        val node = getAllLeafNodes().firstOrNull { it.value >= 10 }
-        if (node != null) {
-            val newValue = split(node.value, node.parent)
-            when (node.direction) {
-                LEFT -> {
-                    node.parent.leftValue = null
-                    node.parent.leftNode = newValue
-                }
-                RIGHT -> {
-                    node.parent.rightValue = null
-                    node.parent.rightNode = newValue
+        companion object {
+
+            @JvmStatic
+            fun fromString(snailfishNumberStr: String) =
+                process(LinkedList(snailfishNumberStr.splitToString())) as SnailfishNumber
+
+            fun split(digit: Int, parent: SnailfishNumber) = SnailfishNumber(
+                parent,
+                floor(digit / 2.0).toInt(),
+                ceil(digit / 2.0).toInt()
+            )
+
+            private fun process(input: LinkedList<String>, parent: SnailfishNumber? = null): Any {
+                val char = input.pop()!!
+                return when {
+                    char == "[" -> {
+                        val snailfishNumber = SnailfishNumber(parent)
+                        snailfishNumber.setLeft(process(input, snailfishNumber))
+                        snailfishNumber.setRight(process(input, snailfishNumber))
+                        snailfishNumber
+                    }
+                    char.isDigits() -> {
+                        if (input.peek().isDigits()) {
+                            (char + input.pop()).toInt()
+                        } else {
+                            char.toInt()
+                        }
+                    }
+                    else -> process(input, parent)
                 }
             }
-            return true
         }
-        return false
-    }
-
-    fun getMagnitude(): Long {
-        val left = if (leftValue != null) leftValue!! * 3L else leftNode!!.getMagnitude() * 3
-        val right = if (rightValue != null) rightValue!! * 2L else rightNode!!.getMagnitude() * 2
-
-        return left + right
-    }
-
-    data class LeafNode(val value: Int, val parent: SnailfishNumber, val direction: Direction)
-    enum class Direction { LEFT, RIGHT }
-
-    private fun getAllLeafNodes(): List<LeafNode> {
-        if (this.parent != null) {
-            return parent!!.getAllLeafNodes()
-        }
-
-        return getChildLeafNodes()
-    }
-
-    private fun getChildLeafNodes(): List<LeafNode> {
-        val nodes = mutableListOf<LeafNode>()
-        if (this.leftNode != null) {
-            nodes.addAll(this.leftNode!!.getChildLeafNodes())
-        }
-        if (this.leftValue != null) {
-            nodes.add(LeafNode(this.leftValue!!, this, LEFT))
-        }
-        if (this.rightNode != null) {
-            nodes.addAll(this.rightNode!!.getChildLeafNodes())
-        }
-        if (this.rightValue != null) {
-            nodes.add(LeafNode(this.rightValue!!, this, RIGHT))
-        }
-
-        return nodes
-    }
-
-    operator fun plus(other: SnailfishNumber) : SnailfishNumber {
-        val newParent = SnailfishNumber(null)
-        newParent.setLeft(this)
-        this.parent = newParent
-        newParent.setRight(other)
-        other.parent = newParent
-
-        return newParent
-    }
-
-    override fun toString(): String {
-        return "[${this.getLeft()},${this.getRight()}]"
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as SnailfishNumber
-
-        if (leftValue != other.leftValue) return false
-        if (rightValue != other.rightValue) return false
-        if (leftNode != other.leftNode) return false
-        if (rightNode != other.rightNode) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = leftValue ?: 0
-        result = 31 * result + (rightValue ?: 0)
-        result = 31 * result + (leftNode?.hashCode() ?: 0)
-        result = 31 * result + (rightNode?.hashCode() ?: 0)
-        return result
     }
 
     companion object {
-        fun fromString(snailfishNumberStr: String) =
-            process(LinkedList(snailfishNumberStr.splitToString())) as SnailfishNumber
-
-        fun split(digit: Int, parent: SnailfishNumber) = SnailfishNumber(
-            parent,
-            floor(digit / 2.0).toInt(),
-            ceil(digit / 2.0).toInt()
-        )
-
-        private fun process(input: LinkedList<String>, parent: SnailfishNumber? = null): Any {
-            val char = input.pop()!!
-            return when {
-                char == "[" -> {
-                    val snailfishNumber = SnailfishNumber(parent)
-                    snailfishNumber.setLeft(process(input, snailfishNumber))
-                    snailfishNumber.setRight(process(input, snailfishNumber))
-                    snailfishNumber
-                }
-                char.isDigits() -> {
-                    if (input.peek().isDigits()) {
-                        (char + input.pop()).toInt()
-                    } else {
-                        char.toInt()
-                    }
-                }
-                else -> process(input, parent)
-            }
-        }
+        private val logger = LoggerFactory.getLogger(Snailfish::class.java)
     }
 }
 
-
 fun main() {
     val input = readInputLines(18)
-    val result = addAndReduceList(input)
-    println(result.getMagnitude())
+    val snailfish = Snailfish()
+
+    val result = snailfish.addAndReduceList(input)
+    println("Magnitude of summed list is: ${result.getMagnitude()}")
+    println("Maximum magnitude is: ${snailfish.findLargestMagnitude(input)}")
 }
--- src/test/kotlin/day18/SnailfishTest.kt
+++ src/test/kotlin/day18/SnailfishTest.kt
@@ -1,16 +1,18 @@
 package day18
 
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.LoggerContext
+import day18.Snailfish.SnailfishNumber
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.CsvSource
-import org.junit.jupiter.params.provider.MethodSource
 import readTestInputLines
+import utils.readInputLines
 
-@Disabled
 class SnailfishTest {
+
+    private val snailfish = Snailfish()
 
 	@Test
     fun `add snailfish numbers`() {
@@ -20,12 +22,6 @@ class SnailfishTest {
         val expected = SnailfishNumber.fromString("[[1,2],[[3,4],5]]")
         assertThat(sn1 + sn2).isEqualTo(expected)
     }
-
-//    @ParameterizedTest(name = "Parses {0}")
-//    @MethodSource("getSnailfishNumbersToParse")
-//    fun `parse snailfish numbers`(string: String, expected: SnailfishNumber) {
-//        assertThat(SnailfishNumber.fromString(string)).isEqualTo(expected)
-//    }
 
     @ParameterizedTest(name = "explodes {0}")
     @CsvSource(value = [
@@ -40,11 +36,10 @@ class SnailfishTest {
         "[[[[6,7],[0,7]],[[7,[6,7]],[0,21]]],[[2,[11,10]],[[0,8],[8,0]]]]|[[[[6,7],[0,7]],[[13,0],[7,21]]],[[2,[11,10]],[[0,8],[8,0]]]]"
         ], delimiter = '|'
     )
-    fun `explode snailfish numbers`(input: String, expected: String) {
-        val snailfishNumber = SnailfishNumber.fromString(input)
+    fun `explode snailfish numbers`(snailfishNumber: SnailfishNumber, expected: SnailfishNumber) {
         val exploded = snailfishNumber.explode()
         assertThat(exploded).isTrue
-        assertThat(snailfishNumber).isEqualTo(SnailfishNumber.fromString(expected))
+        assertThat(snailfishNumber).isEqualTo(expected)
     }
 
     @ParameterizedTest(name = "can split {0}")
@@ -66,43 +61,16 @@ class SnailfishTest {
         assertThat(snailfishNumber).isEqualTo(SnailfishNumber.fromString("[[[[7,7],[7,8]],[[9,5],[8,0]]],[[[9,[5,5]],20],[8,[9,0]]]]"))
     }
 
-    @Test
-    fun `add and reduce`() {
-        val first = SnailfishNumber.fromString("[[[[4,3],4],4],[7,[[8,4],9]]]")
-        val second = SnailfishNumber.fromString("[1,1]")
+    @ParameterizedTest
+    @CsvSource(
+        "[[[[4,3],4],4],[7,[[8,4],9]]]|[1,1]|[[[[0,7],4],[[7,8],[6,0]]],[8,1]]",
+        "[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]|[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]|[[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]",
+        "[[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]|[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]|[[[[7,0],[7,7]],[[7,7],[7,8]]],[[[7,7],[8,8]],[[7,7],[8,7]]]]",
+        "[[[[7,0],[7,7]],[[7,7],[7,8]]],[[[7,7],[8,8]],[[7,7],[8,7]]]]|[7,[5,[[3,8],[1,4]]]]|[[[[7,7],[7,8]],[[9,5],[8,7]]],[[[6,8],[0,8]],[[9,9],[9,0]]]]"
+    , delimiter = '|')
+    fun `add and reduce`(first: SnailfishNumber, second: SnailfishNumber, expected: SnailfishNumber) {
         val added = first + second
-        reduce(added)
-        val expected = SnailfishNumber.fromString("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")
-        assertThat(added).isEqualTo(expected)
-    }
-
-    @Test
-    fun `add and reduce2`() {
-        val first = SnailfishNumber.fromString("[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]")
-        val second = SnailfishNumber.fromString("[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]")
-        val added = first + second
-        reduce(added)
-        val expected = SnailfishNumber.fromString("[[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]")
-        assertThat(added).isEqualTo(expected)
-    }
-
-    @Test
-    fun `add and reduce3`() {
-        val first = SnailfishNumber.fromString("[[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]")
-        val second = SnailfishNumber.fromString("[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]")
-        val added = first + second
-        reduce(added)
-        val expected = SnailfishNumber.fromString("[[[[7,0],[7,7]],[[7,7],[7,8]]],[[[7,7],[8,8]],[[7,7],[8,7]]]]")
-        assertThat(added).isEqualTo(expected)
-    }
-
-    @Test
-    fun `add and reduce4`() {
-        val first = SnailfishNumber.fromString("[[[[7,0],[7,7]],[[7,7],[7,8]]],[[[7,7],[8,8]],[[7,7],[8,7]]]]")
-        val second = SnailfishNumber.fromString("[7,[5,[[3,8],[1,4]]]]")
-        val added = first + second
-        reduce(added)
-        val expected = SnailfishNumber.fromString("[[[[7,7],[7,8]],[[9,5],[8,7]]],[[[6,8],[0,8]],[[9,9],[9,0]]]]")
+        snailfish.reduce(added)
         assertThat(added).isEqualTo(expected)
     }
 
@@ -112,10 +80,9 @@ class SnailfishTest {
         "testInput2.txt|[[[[5,0],[7,4]],[5,5]],[6,6]]",
         "testInput.txt|[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]"
     , delimiter = '|')
-    fun `add and reduce list`(source: String, expected: String) {
-        val result = addAndReduceList(readTestInputLines(18, source))
-        val expectedSnailfishNumber = SnailfishNumber.fromString(expected)
-        assertThat(result).isEqualTo(expectedSnailfishNumber)
+    fun `add and reduce list`(source: String, expected: SnailfishNumber) {
+        val result = snailfish.addAndReduceList(readTestInputLines(18, source))
+        assertThat(result).isEqualTo(expected)
     }
 
     @ParameterizedTest
@@ -123,96 +90,19 @@ class SnailfishTest {
         "[[1,2],[[3,4],5]]|143",
         "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]|3488"
     , delimiter = '|')
-    fun getMagnitude(input: String, expected: Long) {
-        assertThat(SnailfishNumber.fromString(input).getMagnitude()).isEqualTo(expected)
+    fun getMagnitude(input: SnailfishNumber, expected: Long) {
+        assertThat(input.getMagnitude()).isEqualTo(expected)
     }
 
-    companion object {
-//        @JvmStatic
-//        fun getSnailfishNumbersToParse() = listOf(
-//            Arguments.of("[1,2]", SnailfishNumber(0, 1, 2)),
-//            Arguments.of("[[1,2],3]", SnailfishNumber(0, SnailfishNumber(1, 1, 2), 3)),
-//            Arguments.of("[9,[8,7]]", SnailfishNumber(0, 9, SnailfishNumber(1, 8, 7))),
-//            Arguments.of("[[1,9],[8,5]]", SnailfishNumber(0, SnailfishNumber(1, 1, 9), SnailfishNumber(1,8, 5))),
-//            Arguments.of("[[[[1,2],[3,4]],[[5,6],[7,8]]],9]",
-//                SnailfishNumber(0,
-//                    SnailfishNumber(1,
-//                        SnailfishNumber(2,
-//                            SnailfishNumber(3, 1, 2),
-//                            SnailfishNumber(3, 3, 4)
-//                        ),
-//                        SnailfishNumber(2,
-//                            SnailfishNumber(3, 5, 6),
-//                            SnailfishNumber(3, 7, 8)
-//                        )
-//                    ),
-//                    9
-//                )
-//            ),
-//            Arguments.of("[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]",
-//                SnailfishNumber(0,
-//                    SnailfishNumber(1,
-//                        SnailfishNumber(2,
-//                            9,
-//                            SnailfishNumber(3, 3, 8)
-//                        ),
-//                        SnailfishNumber(2,
-//                            SnailfishNumber(3, 0, 9),
-//                            6
-//                        )
-//                    ),
-//                    SnailfishNumber(1,
-//                        SnailfishNumber(2,
-//                            SnailfishNumber(3, 3, 7),
-//                            SnailfishNumber(3, 4, 9)
-//                        ),
-//                        3
-//                    )
-//                )
-//            ),
-//            Arguments.of("[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]",
-//                SnailfishNumber(0,
-//                    SnailfishNumber(1,
-//                        SnailfishNumber(2,
-//                            SnailfishNumber(3,1, 3),
-//                            SnailfishNumber(3,5, 3)
-//                        ),
-//                        SnailfishNumber(2,
-//                            SnailfishNumber(3, 1, 3),
-//                            SnailfishNumber(3, 8, 7)
-//                        )
-//                    ),
-//                    SnailfishNumber(1,
-//                        SnailfishNumber(2,
-//                            SnailfishNumber(3, 4, 9),
-//                            SnailfishNumber(3, 6, 9)
-//                        ),
-//                        SnailfishNumber(2,
-//                            SnailfishNumber(3, 8, 2),
-//                            SnailfishNumber(3, 7, 3)
-//                        )
-//                    )
-//                )
-//            ),
-//            Arguments.of("[[[[0,7],4],[15,[0,13]]],[1,1]]",
-//                SnailfishNumber(0,
-//                    SnailfishNumber(1,
-//                        SnailfishNumber(2,
-//                            SnailfishNumber(3, 0, 7),
-//                            4
-//                        ),
-//                        SnailfishNumber(2,
-//                            15,
-//                            SnailfishNumber(3, 0, 13)
-//                        )
-//                    ),
-//                    SnailfishNumber(1, 1, 1)
-//                ))
-//        )
+    @Test
+    fun `get largest magnitude`() {
+        assertThat(snailfish.findLargestMagnitude(readTestInputLines(18, "testInput3.txt")))
+            .isEqualTo(3993)
+    }
 
-        @JvmStatic
-        fun explode(): List<Arguments> = listOf(
-
-        )
+    @Test
+    fun `get largest magnitude for full input`() {
+        assertThat(snailfish.findLargestMagnitude(readInputLines(18)))
+            .isEqualTo(4685)
     }
 }
